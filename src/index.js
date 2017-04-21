512745637900c55d5c3d3d3f06fd3a4d2d9667ce
Kchalmers <kevinqchal@yahoo.com>
2017-04-20 17:34:41 -0700

    slight changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
-
 import App from './components/app';
+
+
 ReactDOM.render(
-    <Provider >
         <Router history={browserHistory}>
             <Route path="/" component={App}>
 
             </Route>
-        </Router>
-    </Provider>,
+        </Router>,
     document.getElementById('root')
 );
