cd68df2fecda532580afb65d008bd912f50b3f25
stayasleep <virtuallyinfekted@gmail.com>
2017-04-29 15:31:07 -0700

    checks login html against db but doesnt redirect to next page

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const router = express.Router();
+const path = require('path');
 
 //temporary, for now leave the db and connections on the same page
 const connection = mysql.createConnection({
@@ -12,6 +13,32 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-router.
+router.get('/',function(request,response){
+    response.sendFile(path.join(__dirname+'/../skeleton_html/login.html'));
+});
+//test login comparison
+router.post('/login',function(request,response){
+    console.log(request.body);
+    let un = request.body.username;
+    let upw = request.body.password;
+    console.log(un,upw);
+    //call db
+    connection.query("SELECT `user_pw` FROM `users` WHERE `username`=?",[un],function(err,result){
+        if (err) throw err;
+        console.log(result);
+        let str=JSON.stringify(result);
+        let strJ=JSON.parse(str);
+        let checkMate = strJ[0].user_pw;
+        console.log('password db', checkMate);
+        if(checkMate === upw){
+            console.log('the passwords match');
+            response.redirect('/../skeleton_html/home.html');
+        }else{
+            response.statusCode = 404;
+            response.write("404 Sorry Not Found");
+            response.end();
+        }
+    })
+});
 
 module.exports = router;
