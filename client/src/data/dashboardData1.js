3c6adca2a2191e7ebb7c195d17e4830503f57b0a
stayasleep <virtuallyinfekted@gmail.com>
2017-05-02 14:18:29 -0700

    added 3 dd files

---
+++ client/src/data/dashboardData1.js
@@ -0,0 +1,9 @@
+const dashboard={
+    overview: {
+        subject: "Markets",
+        category: "Finance",
+        totalCards: 10,
+        createdBy: "Jonathan",
+        createdOn: "2017-04-30",
+        stackRating: 0.7
+    }};
