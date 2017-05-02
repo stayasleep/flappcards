e31610a5c9e749b0d675188d13757a476d1d7b0c
stayasleep <virtuallyinfekted@gmail.com>
2017-05-02 16:33:20 -0700

    dd for andres

--- server/testApi.js
+++ server/testApi.js
@@ -9,6 +9,13 @@ const bodyParser = require('body-parser');
 
 
 const users = require('./user_data');
+const dashboard=require('./dashboard');
+const stackoverview=require('./stackoverviewMarket');
+const stack2 = require('./stackoverviewCB');
+const myShelf = require('./myShelf');
+const profile = require('./profile');
+const studyMarket = require('./studyMode');
+const studyCB = require('./studyMode2');
 // grab the test users from our test data; it's an object with keys of username.
 // A property of a user is userName: "bleepBloop" which will be used later to check if the user can login
 // userName: "kchalm", user_pw: "Password2"
