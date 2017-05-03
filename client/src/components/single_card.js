961438643efa7e838435074c6d295936cc4206b8
Kchalmers <kevinqchal@yahoo.com>
2017-05-03 11:35:08 -0700

    card effect

--- client/src/components/single_card.js
+++ client/src/components/single_card.js
@@ -13,19 +13,65 @@ class SingleCard extends Component {
     componentWillMount(){
         this.props.getCard(this.props.params.id);
     }
+    componentDidUpdate(){
+        var flip = true;
+        console.log(document);
+        document.getElementById('questionCard').addEventListener('click', switchDisplay);
+        function switchDisplay() {
+            var questionCard = document.getElementById('questionCard');
+            if (flip) {
+                this.classList.toggle('flippedCard');
+                setTimeout(function () {
+                    document.getElementById('question').style.display = 'none';
+                    document.getElementById('answer').style.display = 'block';
+                    flip = false;
+                }, 300);
+            } else {
+                this.classList.toggle('flippedCard');
+                setTimeout(function () {
+                    document.getElementById('question').style.display = 'block';
+                    document.getElementById('answer').style.display = 'none';
+                    flip = true;
+                }, 300);
+            }
+        }
+    }
 
     render() {
+        const cardStyle = {
+                width: '100%',
+                height: '100%',
+                transition: 'transform 1s',
+                display: "block"
+        };
+
+        const answerCard = {
+            display: "none"
+        };
+
+        const flippedCard ={
+            transform: "rotateY(180deg)"
+        };
+
+        const answer ={
+            display: "none",
+        };
+
+        const question ={
+            display: "block"
+        };
+
         const card = this.props.cards[0];
         if(!card){
             return <h3>Loading...</h3>
         }
         return (
             <div>
-                <div className="mdl-card mdl-shadow--2dp " id="questionCard">
-                    <h2 className="mdl-card__title-text " id="question">
+                <div style={cardStyle} className="mdl-card mdl-shadow--2dp " id="questionCard">
+                    <h2 style={question} className="mdl-card__title-text " id="question">
                         {card.question}
                     </h2>
-                    <h2 className="mdl-card__title-text " id="answer">
+                    <h2 style={answer} className="mdl-card__title-text " id="answer">
                         {card.answer}
                         <RaisedButton>Next</RaisedButton>
                     </h2>
