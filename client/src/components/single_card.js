866d44fdfadea86263c836a8f32d761be7a0be27
Kchalmers <kevinqchal@yahoo.com>
2017-05-03 16:24:30 -0700

    next button working

--- client/src/components/single_card.js
+++ client/src/components/single_card.js
@@ -6,16 +6,28 @@ import {getCard} from '../actions/index'
 import RaisedButton from 'material-ui/RaisedButton';
 
 class SingleCard extends Component {
+    constructor(props) {
+        super(props);
+        this.nextCard = this.nextCard.bind(this);
+        this.state = {
+            card: 0
+        }
+    }
+
+    nextCard() {
+        this.setState({ card: this.state.card + 1 })
+    }
+
     static contextTypes ={
         router: PropTypes.object,
     };
+
 
     componentWillMount(){
         this.props.getCard(this.props.params.id);
     }
     componentDidUpdate(){
         var flip = true;
-        console.log(document);
         document.getElementById('questionCard').addEventListener('click', switchDisplay);
         function switchDisplay() {
             var questionCard = document.getElementById('questionCard');
@@ -45,44 +57,43 @@ class SingleCard extends Component {
             textAlign: "center",
             transformStyle: "preserve-3d",
             transformOrigin: "50% 50%",
-            display: "block",
+            display: "flex",
+            justifyContent: "center",
             backfaceVisibility: "visible"
-
         };
         const answerCard = {
             display: "none"
         };
 
         const flippedCard ={
-            transform: "rotateY(180deg)",
+            transform: "rotate(180deg)"
         };
 
         const answer ={
             display: "none",
-            transform: "scale(1)",
+            transform: "scale(1)"
         };
 
         const question ={
             display: "block"
         };
 
-        const card = this.props.cards[0];
+        const onSubmit = this.props;
+        const card = this.props.cards[this.state.card];
         if(!card){
             return <h3>Loading...</h3>
         }
         return (
             <div>
                 <div style={cardStyle} className="mdl-card mdl-shadow--2dp " id="questionCard">
-                    <h2 style={question} className="mdl-card__title-text " id="question">
+                    <h2 style={question} id="question">
                         {card.question}
                     </h2>
-                    <h2 style={answer} className="mdl-card__title-text " id="answer">
+                    <h2 style={answer} id="answer">
                         {card.answer}
-                        <RaisedButton>Next</RaisedButton>
+                        <RaisedButton onClick={() => {this.nextCard()}}>NEXT</RaisedButton>
                     </h2>
-                <div className="mdl-card__menu">
                 </div>
-            </div>
             </div>
         );
     }
