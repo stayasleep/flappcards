5f4c32edb6c2c13f5236c9f724f6d0a01326f204
Kchalmers <kevinqchal@yahoo.com>
2017-05-05 10:12:16 -0700

    pre-merge

--- client/src/components/search/search_page.js
+++ client/src/components/search/search_page.js
@@ -1,63 +1,82 @@
 import React, {Component} from 'react';
 import FlashCardsAppBar from '../appBar/app_bar_with_drawer';
 import {Link} from 'react-router';
+import {Field, reduxForm} from 'redux-form';
+import TextField from 'material-ui/TextField';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui/Table';
 
 class Search extends Component{
+    componentWillMount(){
+        this.props;
+    }
+
+    renderInput({input, label, meta: {touched, error}}){
+        return (
+            <TextField hintText={label}
+                       floatingLabelText={label}
+                       errorText={touched && error}
+                       {...input}
+            />
+        )
+    }
+
     render(){
+
+        // const stacksList = this.props.map((item, index) => {
+        //     return (
+        //         <TableRow key={index}>
+        //             <TableRowColumn>{item.subject}: {item.category}</TableRowColumn>
+        //             <TableRowColumn>{item.totalCards}</TableRowColumn>
+        //             <TableRowColumn>{item.stackRating}</TableRowColumn>
+        //             <TableRowColumn>
+        //                 <Link to="/stackOverview" name="stackOverview"><RaisedButton>
+        //                     View
+        //                 </RaisedButton></Link>
+        //             </TableRowColumn>
+        //             <TableRowColumn>
+        //                 <DeleteStackConfirm/>
+        //             </TableRowColumn>
+        //         </TableRow>
+        //     )
+        // });
 
         return (
             <div>
                 <FlashCardsAppBar/>
-                <div className="mdl-grid">
-                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-                  mdl-textfield--floating-label mdl-textfield--align-right mdl-cell mdl-cell--middle">
-                        <label className="mdl-button mdl-js-button mdl-button--icon"
-                               for="fixed-header-drawer-exp">
-                            <i className="material-icons">search</i>
-                        </label>
-                        <div className="mdl-textfield__expandable-holder">
-                            <input className="mdl-textfield__input" type="text" name="sample"
-                                   id="fixed-header-drawer-exp"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="mdl-grid">
-                    <ul className="demo-list-three mdl-list mdl-cell mdl-cell--center">
-                        <li className="mdl-list__item mdl-list__item--three-line">
-        <span className="mdl-list__item-primary-content">
-          <i className="material-icons mdl-list__item-avatar">functions</i>
-          <span>Physics</span>
-            <span>Physics For Future Presidents</span>
-            <span>Jinwoo</span>
-            <span>Rating 4/5</span>
-          <span className="mdl-list__item-text-body">
-            See Physics decks
-          </span>
-        </span>
-                            <span className="mdl-list__item-secondary-content">
-          <a className="mdl-list__item-secondary-action" href="#"><i className="material-icons">visibility</i></a>
-        </span>
-                        </li>
-                        <li className="mdl-list__item mdl-list__item--three-line">
-        <span className="mdl-list__item-primary-content">
-          <i className="material-icons  mdl-list__item-avatar">functions</i>
-          <span>Physics</span>
-            <span>Physics 7A</span>
-            <span>Chalmers</span>
-            <span>Rating 4/5</span>
-          <span className="mdl-list__item-text-body">
-            See Physics 7A deck
-          </span>
-        </span>
-                            <span className="mdl-list__item-secondary-content">
-          <a className="mdl-list__item-secondary-action" href="#"><i className="material-icons">visibility</i></a>
-        </span>
-                        </li>
-                    </ul>
-                </div>
+                <Field name="Search" component={this.renderInput} label="Search Caegory or Subject"/>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHeaderColumn>Subjects</TableHeaderColumn>
+                            <TableHeaderColumn>Number of Cards</TableHeaderColumn>
+                            <TableHeaderColumn>Rating</TableHeaderColumn>
+                            <TableHeaderColumn>View</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {/*{stacksList}*/}
+                    </TableBody>
+                </Table>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        stacks: state.stack.stacks
+    }
+}
+
+Search = reduxForm({
+    form: 'Search',
+})(Search);
 
 export default Search
