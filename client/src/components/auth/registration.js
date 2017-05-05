4d2eef4a2ad4b5ab2a854ce72ec77ee69980abb8
AndGasper <andy.gasper1@gmail.com>
2017-05-05 15:33:41 -0700

    Updated PropTypes install

--- client/src/components/auth/registration.js
+++ client/src/components/auth/registration.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'; // Updated PropTypes import statement
 import {Field, reduxForm} from 'redux-form';
 import {connect} from 'react-redux';
 import TextField from 'material-ui/TextField';
