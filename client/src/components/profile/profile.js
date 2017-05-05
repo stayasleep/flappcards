b3437517342514b27aefc8d2817b1f8a4afc097a
Kchalmers <kevinqchal@yahoo.com>
2017-05-05 09:15:23 -0700

    profile page is awaiting calls to be made

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
 import FlashCardsAppBar from '../appBar/app_bar_with_drawer';
+import RaisedButton from 'material-ui/RaisedButton';
+import {connect} from 'react-redux'
+import {Link} from 'react-router'
 
 class Profile extends Component{
+    componentWillMount(){
+        this.props;
+    }
+
     render(){
         return (
             <div>
                 <FlashCardsAppBar/>
-                <div>profile page</div>
+                <div>
+                    {this.props.username}
+                </div>
+                <div>
+                    {this.props.email}
+                </div>
+                <div>
+                    {this.props.birthday}
+                </div>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        username: state,
+        email: state,
+        birthday: state
+    }
+}
+
+export default connect(mapStateToProps, {})(Profile);
 
 export default Profile;
