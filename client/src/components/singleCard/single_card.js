a32bc9b3160d13c7bc4af822518977bb366db292
AndGasper <andy.gasper1@gmail.com>
2017-05-06 10:26:21 -0700

    Semi stable version with minimal error handling on server side

--- client/src/components/singleCard/single_card.js
+++ client/src/components/singleCard/single_card.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {Card, CardActions, CardHeader, CardTitle, CardText} from '../../../../node_modules/material-ui/Card';
 import {connect} from 'react-redux'
 import {Link} from 'react-router'
