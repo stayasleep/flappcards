6c87f62bb040d2745b8ebe9658bb9854c1021778
Kchalmers <kevinqchal@yahoo.com>
2017-05-10 16:06:45 -0700

    colors

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -10,16 +10,27 @@ import {
     grey100, grey300, grey400, grey500,
     white, darkBlack, fullBlack,
 } from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+import spacing from 'material-ui/styles/spacing';
 // getMuiTheme() computes a "valid" muiTheme object
 const muiTheme = getMuiTheme({
+    spacing: spacing,
+    fontFamily: 'Roboto, sans-serif',
     palette: {
-        textColor: fullBlack,
         primary1Color: teal300,
         primary2Color: teal500,
         primary3Color: grey400,
         accent1Color: indigo200,
         accent2Color: grey100,
         accent3Color: grey500,
+        textColor: fullBlack,
+        alternateTextColor: white,
+        canvasColor: white,
+        borderColor: grey300,
+        disabledColor: fade(darkBlack, 0.3),
+        pickerHeaderColor: teal300,
+        clockCircleColor: fade(darkBlack, 0.07),
+        shadowColor: fullBlack,
     }
 });
 const App = (props) => (
