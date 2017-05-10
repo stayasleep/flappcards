b7889094b4c7f5557818a8d84487b167d2b5e3fe
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-05-09 17:54:29 -0700

    Prepped for server

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -3,8 +3,8 @@ import FlashCardsAppBar from '../appBar/app_bar_with_drawer';
 import Recent from './recent_stacks'
 import Community from './community_stacks'
 import {Link} from 'react-router'
-import RaisedButton from './../../../../node_modules/material-ui/RaisedButton';
-import Create from './../../../../node_modules/material-ui/svg-icons/content/create';
+import RaisedButton from 'material-ui/RaisedButton';
+import Create from 'material-ui/svg-icons/content/create';
 class Home extends Component {
 
     CreateButton() {
