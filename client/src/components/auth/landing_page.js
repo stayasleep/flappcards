bc0383a97c44a786e17dfb19758862cb1511ceaa
stayasleep <virtuallyinfekted@gmail.com>
2017-05-09 17:13:59 -0700

    unfinished styling on the landing page

--- client/src/components/auth/landing_page.js
+++ client/src/components/auth/landing_page.js
@@ -8,7 +8,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import {Link} from 'react-router'
 import Registration from './registration';
-import Login from './log_in'
+import Login from './log_in';
+import {GridList, GridTile} from 'material-ui/GridList';
 
 class landing extends Component {
     render (){
@@ -16,6 +17,25 @@ class landing extends Component {
             title: {
                 cursor: 'pointer',
             },
+            root:{
+                display:'flex',
+                flexWrap:'wrap',
+                justifyContent:'space-around'
+            },
+            gridList: {
+                width: 500,
+                height: 450,
+                overflowY: 'auto',
+                padding: 4,
+                cols: 2,
+            },
+            titles:{
+                marginBottom: '10vh',
+                marginLeft: '20vw',
+            },
+            midT:{
+                color:'red',
+            }
         };
         const loginStyle = {
             position: "relative",
@@ -29,25 +49,26 @@ class landing extends Component {
             textAlign: "center",
             fontSize: "4vh"
         };
+
         return (
             <div>
-                <Toolbar>
+                <Toolbar style={styles.header}>
                     <ToolbarTitle text="FlappCards" />
                     <ToolbarGroup>
                         <Login/>
                     </ToolbarGroup>
                 </Toolbar>
                 <div>
-                        FLAPPCARDS<br/>
-                        The #1 Flashcard-App Around!<br/>
+                    <h1 style={styles.titles}>FL<span style={styles.midT}>APP</span>CARDS</h1>
+                    <h3>The #1 Flashcard-App Around!</h3><br/>
 
-                        Create personalized study stacks or search new topics at the click of a button.<br/>
+                    <h5>Create personalized study stacks or search new topics at the click of a button.</h5><br/>
 
-                    Our mission is to make learning easier for everybody.<br/>
-                    Improve your grades by studying with FlappCards.  It's easy to use, fun, and always free!<br/>
+                    <h5>Our mission is to make learning easier for everybody.</h5><br/>
+                    <h5>Improve your grades by studying with FlappCards.  It's easy to use, fun, and always free!</h5><br/>
 
-                    Find more of what you're looking for on FlappCards,<br/>
-                        Join Today!
+                    <h6>Find more of what you're looking for on FlappCards,</h6><br/>
+                    <h6>Join Today!</h6>
                     <Registration/>
                 </div>
             </div>
