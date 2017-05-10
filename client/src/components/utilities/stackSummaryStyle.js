21705650f5fa2c34dec9f5f841ace3a1a87ac369
AndGasper <andy.gasper1@gmail.com>
2017-05-10 15:51:44 -0700

    Recent and community stacks are now returned from a renderStackSummary component

---
+++ client/src/components/utilities/stackSummaryStyle.js
@@ -0,0 +1,29 @@
+
+export const subHeader = {
+        textAlign: "center",
+        fontSize: "2em",
+        fontWeight: "bold",
+        fontFamily: "Roboto, sans-serif",
+        paddingLeft: 0
+};
+
+export const cardHeader = {
+        fontSize: "1em",
+        fontWeight: "bold"
+};
+
+export const cardActions ={
+        position: "relative",
+        display: "inline-flex",
+        float: "right",
+        marginRight: "1em"
+};
+
+export const cardText = {
+        fontSize: "1em"
+};
+
+export const cardDisplay = {
+        marginTop: "1em",
+        marginBottom: "1em"
+};
