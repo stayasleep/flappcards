4a3d2359a829de96b93dbb57c528e0bf0b32bd46
stayasleep <virtuallyinfekted@gmail.com>
2017-06-12 02:02:35 -0700

    email pw reset testing works on postman. need to encrypt password on reset and then things appear to be looking fine.

--- routes/recovery.js
+++ routes/recovery.js
@@ -22,63 +22,98 @@ router.post('/',(req,res,next)=>{
             }else{
                 if(results.length === 1){
                     console.log('res ',results);
-                    console.log('array', results[0]);
-                    console.log('length ',results.length);
-                    console.log('req',req);
-                    console.log('ip',req.ip);
-                    console.log('proto',req.protocol);
-                    console.log('host',req.hostname);
+                    // console.log('req',req);
+                    // console.log('proto',req.protocol);
+                    // console.log('host',req.hostname);
                     //results from db
                     let recoveryEmail = results[0].user_email;
-                    let recoveryName = results[0].userName;
+                    let recoveryName = results[0].username;
                     //SHOULD GENERATE TOKEN HERE
                     let token = jwt.sign({UserName: recoveryName, UserEmail: recoveryEmail},special.secret,{
                         expiresIn: 14400 // expires 4hrs after generated
                     });
                     //set the token on the db as a preventative measure
                     connection.query(
-                        "UPDATE `users` SET `reset_token` = ? WHERE `user_name` = ?;",[token,recoveryName],(err,results)=>{
+                        "UPDATE `users` SET `reset_token` = ? WHERE `username` = ?;",[token,recoveryName],(err,results)=>{
                             if(err){
-                                return res.json({success: false, message:"There was a problem with your request"});
+                                return res.send({success: false, message:"There was a problem with your request"});
                             }
+                            //so there must be success
+                            //transporter is an obj that is able to send mail, only need to create this one
+                            let transporter = nodemailer.createTransport({
+                                host:'smtp.gmail.com',  // Specify main and backup SMTP servers
+                                port: 587,              // TCP port to connect to
+                                secure: false,         // secure:true for port 465, secure:false for port 587
+                                auth:{
+                                    user: mailer.user,
+                                    pass: mailer.pass
+                                }
+                            });
+                            // verify connection configuration
+                            transporter.verify(function(error, success) {
+                                if (error) {
+                                    console.log(error);
+                                } else {
+                                    console.log('Server is ready to take our messages');
+                                }
+                            });
+                            //mail configuration
+                            let mailOptions = {
+                                from:'"FlappCard Support" <'+mailer.user+'>',  //us
+                                to: "",                                    //response from db
+                                subject:"Account Recovery",
+                                text:"Your account reset recovery link is below",
+                                html:"<b>Hello B!</b> Your account recovery link is <em>below</em>\n\n"+
+                                req.protocol+"://"+req.hostname+"/reset/"+token
+
+                            };
+                            //combine mail content with transporter obj and send
+                            transporter.sendMail(mailOptions,(err,info)=>{
+                                if(err){
+                                    return res.json({success:false, message:"There was a problem with the delivery. Please try again later."});
+                                }
+                                console.log('log this info',info);
+                            });
+                            return res.json({success:true, message:"An email has been sent to the provided email address, thank you."});
+
                         }
                     );
-                    //transporter is an obj that is able to send mail, only need to create this one
-                    let transporter = nodemailer.createTransport({
-                        host:'smtp.gmail.com',  // Specify main and backup SMTP servers
-                        port: 587,              // TCP port to connect to
-                        secure: false,         // secure:true for port 465, secure:false for port 587
-                        auth:{
-                            user: mailer.user,
-                            pass: mailer.pass
-                        }
-                    });
-                    // verify connection configuration
-                    transporter.verify(function(error, success) {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            console.log('Server is ready to take our messages');
-                        }
-                    });
-                    //mail configuration
-                    let mailOptions = {
-                        from:'"FlappCard Support" <'+mailer.user+'>',  //us
-                        to: "",                                    //response from db
-                        subject:"Account Recovery",
-                        text:"Your account reset recovery link is below",
-                        html:"<b>Hello B!</b> Your account recovery link is <em>below</em>\n\n"+
-                            req.protocol+"://"+req.hostname+"/reset/"+token
-
-                    };
-                    //combine mail content with transporter obj and send
-                    transporter.sendMail(mailOptions,(err,info)=>{
-                        if(err){
-                            return res.json({success:false, message:"There was a problem with the delivery. Please try again later."});
-                        }
-                        console.log('log this info',info);
-                    });
-                    res.json({success:true, message:"An email has been sent to the provided email address, thank you."});
+                    // //transporter is an obj that is able to send mail, only need to create this one
+                    // let transporter = nodemailer.createTransport({
+                    //     host:'smtp.gmail.com',  // Specify main and backup SMTP servers
+                    //     port: 587,              // TCP port to connect to
+                    //     secure: false,         // secure:true for port 465, secure:false for port 587
+                    //     auth:{
+                    //         user: mailer.user,
+                    //         pass: mailer.pass
+                    //     }
+                    // });
+                    // // verify connection configuration
+                    // transporter.verify(function(error, success) {
+                    //     if (error) {
+                    //         console.log(error);
+                    //     } else {
+                    //         console.log('Server is ready to take our messages');
+                    //     }
+                    // });
+                    // //mail configuration
+                    // let mailOptions = {
+                    //     from:'"FlappCard Support" <'+mailer.user+'>',  //us
+                    //     to: "bernstein.brianj@gmail.com",                                    //response from db
+                    //     subject:"Account Recovery",
+                    //     text:"Your account reset recovery link is below",
+                    //     html:"<b>Hello B!</b> Your account recovery link is <em>below</em>\n\n"+
+                    //         req.protocol+"://"+req.hostname+"/reset/"+token
+                    //
+                    // };
+                    // //combine mail content with transporter obj and send
+                    // transporter.sendMail(mailOptions,(err,info)=>{
+                    //     if(err){
+                    //         return res.json({success:false, message:"There was a problem with the delivery. Please try again later."});
+                    //     }
+                    //     console.log('log this info',info);
+                    // });
+                    // res.json({success:true, message:"An email has been sent to the provided email address, thank you."});
                 }else{
                     res.json({success:false, message:"Username/Email not found"});
                 }
--- routes/reset.js
+++ routes/reset.js
@@ -4,17 +4,20 @@ const mysql = require('mysql');
 const pool = require('../config/config');
 const jwt = require('jsonwebtoken');
 const config = require('../config/helena');
-router.get('/:token',(req,res,next)=>{
+router.use('/:token',(req,res,next)=>{
     //we begin by checking that the link they clicked hasnt expired yet
     //since this route has a shorter exp and a new secret, it will have sep middleware
     const token = req.params.token;
     jwt.verify(token,config.secret,(err,decoded)=>{
         if(err){
-            return res.json({success:false,message:"Failed to authenticate."});
+            res.json({success:false,message:"Failed to authenticate."});
         }else{
             //token is good, it should show the new pw confirmation page
             //render route or something?
-            req.decoded = token;
+            req.decoded = decoded;
+            next();
+            // res.json({success:true, message:"Continue"});
+
         }
     })
 });
@@ -23,14 +26,17 @@ router.post('/:token',(req,res,next)=>{
     //we should check to see if the token is still valid, on the chance that
     //the browser stayed open but had 0 activity, we check the exp
     let checkExp = req.decoded.exp;
+    console.log('decode nuts',req.decoded);
     let timeNow = Math.floor(Date.now()/1000);
     if(!(checkExp >=timeNow)){
         //token has expired so they should get an error message and then be redirected to landing page
         return res.json({success:false, message:"Token is no longer valid. Please try your request again"});
     }
-    let checkToken = req.decoded;
+    let checkToken = req.params.token;
     let un = req.decoded.UserName;
     let newPw = req.body.resetPw;
+    console.log('un',un);
+    console.log('tok',checkToken);
     //token is still good to use lets check with the db to make sure it is the right user
     pool.getConnection((err,connection)=>{
         if (err){
@@ -38,7 +44,7 @@ router.post('/:token',(req,res,next)=>{
         }
         connection.query("SELECT username, reset_token FROM `users` WHERE `reset_token`=? AND username =?;",[checkToken,un],(err,results)=>{
             if(err){
-                return res.json({success: false, message:"There was a problem with your request"});
+                res.json({success: false, message:"There was a problem with your request"});
             }else if(results.length === 1){
                 connection.query(
                     "UPDATE `users` SET `reset_token`= ?, user_pw = ? WHERE `username`=?;",[null,newPw,un],(err,results)=>{
@@ -51,12 +57,13 @@ router.post('/:token',(req,res,next)=>{
                             //redirect to log in page or landing page? Invalidate old token
                             //if they get sent to log in, the token is never used again: do nada
                             //if they get sent to /home then they need a token sent to them
+                            res.json({success:true, message:'we did it'})
                         }
                     }
                 )
             }else{
                 //attempt query but there is no match
-                return res.json({success: false, message:"Unable to perform request, no match found."})
+                res.json({success: false, message:"Unable to perform request, no match found."})
             }
         })
     })
