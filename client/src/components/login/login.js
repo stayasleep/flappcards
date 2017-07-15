94ac3f7148af15aefb3a61afa106f31df23edf8d
stayasleep <virtuallyinfekted@gmail.com>
2017-07-15 03:49:08 -0700

    this component looks real nice compared to my other ones

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import {Field, reduxForm} from 'redux-form';
 import {connect} from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import {Link} from 'react-router';
 import renderInput from '../utilities/renderInput';
 import {userLogin} from '../../actions/index';
 import RecoverPw from '../confirmActionModal/recoverPW';
@@ -19,12 +21,35 @@ class SignIn extends Component{
         //Action creator userLogin sends an axios call to the server
         this.props.userLogin(values);
     }
+  
+    componentDidMount(){
+        //this page has less content so the footer is naturally high up until we manually change that
+        let footer = document.getElementsByClassName("footer")[0];
+        console.log('foot',footer);
+        footer.style.position = "absolute";
+        footer.style.bottom = "10px";
+        footer.style.left="8px";
+        footer.style.right="8px";
+        //center the paper on this one component
+        let paperBody = document.getElementsByClassName("paperBody")[0];
+        paperBody.style.marginTop="3em";
+    }
+
+    componentWillUnmount(){
+        console.log("hi");
+    }
 
 
     render(){
         const {handleSubmit, error} = this.props;
         return(
             <div>
+                <Toolbar className="navHeader">
+                    <ToolbarTitle text={<a  className="navTitleBar" href="https://flappcards.com">FlappCards</a>}/>
+                    <ToolbarGroup>
+                        <RaisedButton labelColor="rgb(0, 121, 107)" label="Home" containerElement={<Link to="/home"/>}/>
+                    </ToolbarGroup>
+                </Toolbar>
                 <Paper className="paperBody" zDepth={2}>
                     <div className="innerPaper">
                         <h1 className="titleUnderline">Log In</h1>
